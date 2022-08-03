98cb467690a9697194eaa0d9207be013aa6ceec1
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-08-03 20:44:09 +0900

    feat: 페이지 라우팅 구현
    - react-router-dom 라이브러리 사용
    - ‘/‘, ‘user/login’, ‘user/logout’, ‘img/analysis’ ,  페이지 생성
    
    Closes #1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// Page
+import Main from './pages/Main';
+import LogIn from './pages/LogIn';
+import LogOut from './pages/LogOut';
+import Analysis from './pages/Analysis';
 
 function App() {
   return (
-    <div>App component!</div>
+    <Routes>
+      
+      {/* Main */}
+      <Route path='/' element={<Main />} />
+
+      {/* User */}
+      <Route path='user'>
+        <Route path='login' element={ <LogIn /> }/>
+        <Route path='logout' element={ <LogOut /> }/>
+      </Route>
+      
+      {/* Img */}
+      <Route path='img'>
+        <Route path='analysis' element={ <Analysis /> }/>
+      </Route>
+
+    </Routes>
   );
 }
 
---
+++ src/pages/LogIn/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LogIn = () => {
+
+  return (
+    <div>LogIn Page!</div> 
+  );
+}
+
+export default LogIn;
