e7ad8e9d6b19d813cdc47931ed89bd426a24e1c1
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-08-07 18:34:58 +0900

    style: 공통적으로 사용되는 컴포넌트 생성
    - styled-components의 ts 사용을 위해 @types/styled-components 모듈 추가
    - FlexBox.style.ts: flex를 사용하여 정렬하기 위한 컴포넌트
    - CenterLayout.style.ts: 기존 요소 위치에서 벗어나 레이아웃을 잡기 위한 컴포넌트
    - CenterBox.tsx: 위의 두 컴포넌트를 조합하여 만든 컴포넌트
    - Button.style.ts: 기본 버튼 컴포넌트
    - index.css: reset css 추가

---
+++ src/style/Button.style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  outline: none;
+  border: none;
+
+  & {
+    cursor: pointer;
+  }
+`;
---
+++ src/style/CenterLayout.style.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+interface CenterLayoutProps {
+  background?: string;
+}
+
+export const CenterLayout = styled.div<CenterLayoutProps>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  
+  width: 100%;
+  height: 100%;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  background: ${ ({ background }) => background || 'none' }
+`;
