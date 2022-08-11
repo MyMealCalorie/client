784eb36b6b934451c2996232503ede644feb422d
hyowonii <hyowon9964@gmail.com>
2022-08-11 15:37:25 +0900

    style: 이미지 분석 페이지 UI 구현

--- src/style/FlexBox.style.ts
+++ src/style/FlexBox.style.ts
@@ -1,10 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 interface FlexBoxProps {
   background?: string;
 
   width?: string;
   height?: string;
+  margin?: string;
 
   flexDirection?: string;
   justifyContent?: string;
@@ -12,13 +13,14 @@ interface FlexBoxProps {
 }
 
 export const S_FlexBox = styled.div<FlexBoxProps>`
-  width: ${ props => props.width };
-  height: ${ props => props.height };
+  width: ${props => props.width};
+  height: ${props => props.height};
+  margin: ${props => props.margin};
 
   display: flex;
-  flex-direction: ${ props => props.flexDirection || 'row'};
-  justify-content: ${ props => props.justifyContent || 'center' };
-  align-items: ${ props => props.alignItems || 'center'};
+  flex-direction: ${props => props.flexDirection || 'row'};
+  justify-content: ${props => props.justifyContent || 'center'};
+  align-items: ${props => props.alignItems || 'center'};
 
-  background: ${ props => props.background || 'none' }; 
+  background: ${props => props.background || 'none'};
 `;
