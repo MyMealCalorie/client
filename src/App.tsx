a89fcc7d8a160f6e05f2805b5326b738753cf2ef
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-08-12 10:23:28 +0900

    feat: 라우터 경로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,27 @@ import Main from './pages/Main';
 import LogIn from './pages/LogIn';
 import SignUp from './pages/SignUp';
 import Analysis from './pages/Analysis';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Routes>
-      
+      {/* Index */}
+      <Route path="/" element={<Home />} />
+
       {/* Main */}
-      <Route path='/' element={<Main />} />
+      <Route path="main" element={<Main />} />
 
       {/* User */}
-      <Route path='user'>
-        <Route path='login' element={ <LogIn /> }/>
-        <Route path='signup' element={ <SignUp /> }/>
-      </Route>
-      
-      {/* Img */}
-      <Route path='img'>
-        <Route path='analysis' element={ <Analysis /> }/>
+      <Route path="user">
+        <Route path="login" element={<LogIn />} />
+        <Route path="signup" element={<SignUp />} />
       </Route>
 
+      {/* Img */}
+      <Route path="img">
+        <Route path="analysis" element={<Analysis />} />
+      </Route>
     </Routes>
   );
 }
