47a93f8ee23b7b4984be463ba085067068a01c8f
hyowonii <hyowon9964@gmail.com>
2022-08-13 23:03:03 +0900

    style: 달력 디자인

--- src/components/Main/CalendarContainer.tsx
+++ src/components/Main/CalendarContainer.tsx
@@ -1,5 +1,5 @@
 import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import './style/calendar.css';
 import { S_FlexBox } from '../../style/FlexBox.style';
 
 interface Props {
