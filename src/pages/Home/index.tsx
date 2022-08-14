151d7819bfeac595016205392c7251ec9f35039e
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-08-14 20:48:07 +0900

    chore: 이미지 ts 오류 해결로 인한 경로 변경

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import Header from '../../components/common/Header';
 import { S_FlexBox } from '../../style/FlexBox.style';
 
 import Main from '../../components/Home/Main';
+import CARROTS from '../../assets/carrots.jpg';
 
 const Home = () => {
   return (
@@ -12,7 +13,7 @@ const Home = () => {
 
       {/* Main 이미지, 멘트 영역 */}
       <Main
-        bgImg="carrots.jpg"
+        bgImg={CARROTS}
         title="내 밥 몇 칼로리?"
         ment=" 사진 한 장으로 손쉽게 나의 섭취 칼로리를 계산하고, 관리해 보세요!"
       />
