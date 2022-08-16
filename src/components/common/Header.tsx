6d3867490f5ed8a152bb107b09876a2ac633ff2e
hyowonii <hyowon9964@gmail.com>
2022-08-16 13:01:18 +0900

    feat: 로그아웃

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -10,13 +10,27 @@ const Header = () => {
     sessionStorage.getItem('loggedInfo') as string
   );
 
+  const onLogout = () => {
+    const logout = window.confirm('로그아웃 하시겠습니까?');
+
+    if (logout) {
+      sessionStorage.removeItem('loggedInfo');
+      alert('로그아웃 되었습니다.');
+      window.location.reload();
+    }
+  };
+
   return (
     <S_HeaderWrapper>
       <S_MainLogo src={Logo} onClick={() => navigate('/')} /> {/*로고*/}
       {!sessionLoggedInfo ? (
         <></>
       ) : (
-        <MdLogout size="1.5rem" style={{ cursor: 'pointer' }} />
+        <MdLogout
+          size="1.5rem"
+          style={{ cursor: 'pointer' }}
+          onClick={onLogout}
+        />
       )}
     </S_HeaderWrapper>
   );
