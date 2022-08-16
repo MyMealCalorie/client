f8f3b7fcff07f723a6ad171791b96804dc4df19b
hyowonii <hyowon9964@gmail.com>
2022-08-16 11:07:09 +0900

    style: 헤더 우측 로그인 유무에 따른 로그아웃 버튼 배치

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,14 +1,23 @@
 import { S_HeaderWrapper, S_MainLogo } from './style';
 import Logo from './../../assets/main_logo.png';
 import { useNavigate } from 'react-router-dom';
+import { MdLogout } from 'react-icons/md';
 
 const Header = () => {
   const navigate = useNavigate();
 
+  const sessionLoggedInfo = JSON.parse(
+    sessionStorage.getItem('loggedInfo') as string
+  );
+
   return (
     <S_HeaderWrapper>
       <S_MainLogo src={Logo} onClick={() => navigate('/')} /> {/*로고*/}
-      <button>Login</button> {/*로그인*/}
+      {!sessionLoggedInfo ? (
+        <></>
+      ) : (
+        <MdLogout size="1.5rem" style={{ cursor: 'pointer' }} />
+      )}
     </S_HeaderWrapper>
   );
 };
